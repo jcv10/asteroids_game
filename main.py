f4b3247784be233287d2ed9e6d98b1277f55a891
Julio Velasco <juliocvelasco90@gmail.com>
2025-03-11 12:33:51 -0400

    Render player unto the screen

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen =pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
-    print('Starting Asteroids!')
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ =='__main__':
     main()
