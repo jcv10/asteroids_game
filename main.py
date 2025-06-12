cec8cdbfa4c0f3c0a7998967347edb893a6c2985
Julio Velasco <juliocvelasco90@gmail.com>
2025-06-12 13:03:11 -0400

    Added player and shoot ability

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
 import pygame
+import sys
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
     screen =pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    AsteroidField.containers = updatable
+    Asteroid.containers = (asteroids,updatable,drawable)
+    Player.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
         pygame.Surface.fill(screen, (0,0,0))
-        player.update(dt)
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
